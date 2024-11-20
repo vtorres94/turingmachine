09a90e36f9c47f5c627b98e3c2b68234a1e05980
Vladimir Torres <vladimir.tordavi@gmail.com>
2024-11-20 01:53:31 -0600

    upgrade versions with pnpm

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import TuringMachine from './components/TuringMachine';
+import TuringMachine from './components/TuringMachine.tsx';
 
 function App() {
-  const [ ww, setWw ] = useState(window.innerWidth);
-    
-  useEffect(()=>{
+  const [ww, setWw] = useState(window.innerWidth);
+
+  useEffect(() => {
     window.addEventListener("resize", () => setWw(typeof window !== "undefined" ? window.innerWidth : 0))
   }, []);
   return (
     <div className="App">
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" style={{ marginLeft: ww > 700 ? '35%' : '22%'}}/>
-        <TuringMachine ww={ww}/>
+        <img src={logo} className="App-logo" alt="logo" style={{ marginLeft: ww > 700 ? '35%' : '22%' }} />
+        <TuringMachine ww={ww} />
       </header>
     </div>
   );
